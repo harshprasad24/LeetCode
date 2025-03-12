ec19decfbb16ebbe674e03aad42721f2dfbbc2d1
Harsh Prasad <harshprasad2408@gmail.com>
2025-03-13 00:04:35 +0530

    Add files via upload

---
+++ Search2d.java
@@ -0,0 +1,18 @@
+public class Search2d {
+    public static void main(String[] args) {
+        int matrix[][] = {{1,4,7,11,15},{2,5,8,12,19},{3,6,9,16,22},{10,13,14,17,24},{18,21,23,26,30}};
+        int target = 20;
+        boolean ans = searchMatrix(matrix, target);
+        System.out.println(ans);
+    }
+    public static boolean searchMatrix(int[][] matrix, int target) {
+        for(int i=0;i<matrix.length;i++){
+            for(int j=0;j<matrix[0].length;j++){
+                if(matrix[i][j]==target){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+}
