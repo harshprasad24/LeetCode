1a1400f5e2755445cae433674c27126b434f5baf
Harsh Prasad <harshprasad2408@gmail.com>
2025-04-12 15:26:55 +0530

    Add files via upload

---
+++ DuplicateNumber287.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class DuplicateNumber287 {
+    public static void main(String[] args) {
+        int nums[] = {1,2,5,2,3};
+        int ans = findDuplicate(nums);    
+        System.out.print(ans);                              
+    }
+    public static int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==nums[i-1]){
+                return nums[i];
+            }
+        }
+        return -1;
+    }
+}
