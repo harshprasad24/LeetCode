5106914d75e61e65c3de17c84c0ca55600d527ac
Harsh Prasad <harshprasad2408@gmail.com>
2025-05-12 12:21:00 +0530

    Add files via upload

---
+++ ThirdMaximumNumber414.java
@@ -0,0 +1,25 @@
+public class ThirdMaximumNumber414 {
+    public static void main(String[] args) {
+        int nums[]= {1,1,2};
+        System.out.println(thirdMax(nums));
+    }
+    public static int thirdMax(int[] nums) {
+        long first = Long.MIN_VALUE, second = Long.MIN_VALUE, third = Long.MIN_VALUE;
+
+        for (int num : nums) {
+            if (num == first || num == second || num == third) continue;
+
+            if (num > first) {
+                third = second;
+                second = first;
+                first = num;
+            } else if (num > second) {
+                third = second;
+                second = num;
+            } else if (num > third) {
+                third = num;
+            }
+        }
+        return (third == Long.MIN_VALUE) ? (int) first : (int) third;
+    }
+}
